f225bce4661a3a7618ebf8552c2d495e31e5d4c1
nick.serebrennikov <nikolay.serebrennikov@onetwotrip.com>
2016-06-22 22:51:53 +0300

    added tests for several trivial cases

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const readline = require('readline');
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
 const fileName = '_bcb5c6658381416d19b01bfc1d3993b5_IntegerArray.txt';
 const pathName = path.join('./', fileName);
-const countInversions = require('./count-inversions')
-const arr = []
+const countInversions = require('./count-inversions');
+const arr = [];
+
+const counter = function (arr) {
+    console.log('Counting Number of inversions in ' + arr.length);
+    const numInversions = countInversions(arr);
+    console.log('Number of inversions: ' + numInversions);
+};
 
 const rl = readline.createInterface({
     input: fs.createReadStream(pathName)
@@ -14,7 +20,5 @@ rl.on('line', function (line) {
     arr.push(parseInt(line));
 });
 rl.on('close', function () {
-    console.log('Counting Number of inversions in ' + arr.length);
-    const numInversions = countInversions(arr)
-    console.log('Number of inversions: ' + numInversions);
+    counter(arr);
 });
