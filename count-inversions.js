8c3ee0b90319fffb3f59e80ac0b5267b361fbfa2
nick.serebrennikov <nikolay.serebrennikov@onetwotrip.com>
2016-06-22 23:08:42 +0300

    it obviously dows not work without of merge piggyback

--- count-inversions.js
+++ count-inversions.js
@@ -1,15 +1,19 @@
-function countSplitInversions(left, right) {
+function countSplitInversions(arr, left, right) {
     var i = 0;
     var j = 0;
     var count = 0;
     while (i < left.length || j < right.length) {
         if (i == left.length) {
+            arr[i + j] = right[j];
             j++;
         } else if (j == right.length) {
+            arr[i + j] = left[i];
             i++;
         } else if (left[i] <= right[j]) {
+            arr[i + j] = left[i];
             i++;
         } else {
+            arr[i + j] = right[j];
             count += left.length - i;
             j++;
         }
@@ -24,5 +28,5 @@ module.exports = function countInversions(arr) {
     const midIdx = Math.floor((arr.length + 1) / 2);
     const left = arr.slice(0, midIdx);
     const right = arr.slice(midIdx);
-    return countInversions(left) + countInversions(right) + countSplitInversions(left, right);
+    return countInversions(left) + countInversions(right) + countSplitInversions(arr, left, right);
 };
--- tests.js
+++ tests.js
@@ -3,6 +3,7 @@ const countInversions = require('./count-inversions');
 const assert = require('assert');
 
 assert.equal(0, countInversions([]));
+assert.equal(0, countInversions([0,1]));
 assert.equal(0, countInversions([0,1,2,3]));
 assert.equal(1, countInversions([0,1,3,2]));
 assert.equal(1, countInversions([3,1,2,0]));
