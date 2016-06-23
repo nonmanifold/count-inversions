d838f409839f4c199a39823f8ac0dab4eb03ab73
nick serebrennikov <non.manifold@gmail.com>
2016-06-23 22:33:46 +0300

    added test cases

--- tests.js
+++ tests.js
@@ -3,8 +3,13 @@ const countInversions = require('./count-inversions');
 const assert = require('assert');
 
 assert.equal(0, countInversions([]));
-assert.equal(0, countInversions([0,1]));
-assert.equal(0, countInversions([0,1,2,3]));
-assert.equal(1, countInversions([0,1,3,2]));
-assert.equal(1, countInversions([3,1,2,0]));
-assert.equal(4, countInversions([3,2,1,0]));
+assert.equal(0, countInversions([1, 2]));
+assert.equal(0, countInversions([1, 2, 3, 4]));
+assert.equal(3, countInversions([1, 3, 5, 2, 4, 6]));
+assert.equal(1, countInversions([1, 3, 2, 4]));
+assert.equal(2, countInversions([3, 1, 2]));
+assert.equal(3, countInversions([3, 2, 1]));
+assert.equal(6, countInversions([4, 2, 3, 5, 1]));
+assert.equal(6, countInversions([4, 3, 2, 1]));
+assert.equal(5 * (5 - 1) / 2, countInversions([5, 4, 3, 2, 1]));
+console.log('Pass');
